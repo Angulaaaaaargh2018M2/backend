42b4d21ea553f25ad21aec7035d879075d2e3fc8
njl07 <akanass@icloud.com>
2018-11-05 15:41:06 +0100

    step-01-solution

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 // * NOTE :: leave this until @hapiness/core will be migrated to rxjs v6 - This library is installed automatically
 import 'rxjs-compat';
+import { Hapiness } from '@hapiness/core';
+
+import { ApplicationModule } from './application.module';
+
+// bootstrap application
+Hapiness.bootstrap(ApplicationModule)
+    .catch(err => console.log(err));
