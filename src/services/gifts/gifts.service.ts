380db141a3a09c1b94bf562e8fa11c93674b4c30
Simon HAJEK <simonhajek88@gmail.com>
2018-11-16 14:10:01 +0100

    Create service and doc

--- src/services/gifts/gifts.service.ts
+++ src/services/gifts/gifts.service.ts
@@ -1,6 +1,8 @@
-import {Injectable} from '@hapiness/core';
+import {HTTPHandlerResponse, Injectable} from '@hapiness/core';
 import {GiftsDocumentService} from './gifts-document.service';
-import {Observable} from 'rxjs';
+import {Observable, of, throwError} from 'rxjs';
+import {catchError, flatMap, map} from 'rxjs/operators';
+import {Biim} from '@hapiness/biim';
 
 
 @Injectable()
@@ -16,5 +18,61 @@ export class GiftsService {
      */
     listAll(): Observable<Gift[] | void> {
         return this._giftsDocumentService.find();
+    }
+
+    /**
+     * Returns one gift of the list matching id in parameter
+     *
+     * @param {string} id of the gift
+     *
+     * @returns {Observable<Gift>}
+     */
+    one(id: string): Observable<Gift> {
+        return this._giftsDocumentService.findById(id)
+            .pipe(
+                catchError(e => throwError(Biim.preconditionFailed(e.message))),
+                flatMap(_ =>
+                    !!_ ?
+                        of(_) :
+                        throwError(Biim.notFound(`Gift with id '${id}' not found`))
+                )
+            );
+    }
+
+    // TODO CHECK PAR NAME ET PAR EVENTID
+    /**
+     * Check if gift already exists (by name and by enventID) and add it in gifts list
+     *
+     * @param gift to create
+     *
+     * @returns {Observable<HTTPHandlerResponse>}
+     */
+    create(gift: Gift): Observable<HTTPHandlerResponse> {
+        return this._addGift(gift)
+            .pipe(
+                flatMap(_ => this._giftsDocumentService.create(_)),
+                catchError(e =>
+                    e.code = 11000 ?
+                        throwError(
+                            Biim.conflict(`Gift with name '${gift.name}' and for eventID '${gift.eventId}' already exists`)
+                        ) :
+                        throwError(Biim.preconditionFailed(e.message))
+                ),
+                map(_ => ({ response: _, statusCode: 201 }))
+            );
+    }
+
+
+    /**
+     * Add gift with good data in gifts list
+     *
+     * @param gift to add
+     *
+     * @returns {Observable<any>}
+     *
+     * @private
+     */
+    private _addGift(gift: Gift): Observable<any> {
+        return of(gift);
     }
 }
