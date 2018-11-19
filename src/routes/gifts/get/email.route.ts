43d26cb608218db94c95102aaa178b86e7201275
Simon HAJEK <simonhajek88@gmail.com>
2018-11-19 16:23:02 +0100

    Mails for one gift

--- src/routes/gifts/get/email.route.ts
+++ src/routes/gifts/get/email.route.ts
@@ -1,34 +1,42 @@
 import {OnGet, Request, Route} from '@hapiness/core';
-import {Observable, of} from 'rxjs';
-import {GIFT_RESPONSE} from '../../../schemas';
+import {Observable} from 'rxjs';
+import {ID_PARAMETER} from '../../../schemas';
 import {MailsService} from '../../../services/mails.service';
+import {tap} from 'rxjs/operators';
+import {LoggerService} from '@hapiness/logger';
 
 
 @Route({
-    path: '/api/gifts/email',
+    path: '/api/gifts/{id}/email',
     method: 'GET',
     config: {
-        response: {
-            status: {
-                200: GIFT_RESPONSE
+        validate: {
+            params: {
+                id: ID_PARAMETER
             }
         },
-        description: 'Get one gift',
-        notes: 'Returns one gift for the given id in path parameter',
+        response: {
+            status: {
+                200: undefined
+            }
+        },
+        description: 'Send email for one gift to all link email from the gift',
+        notes: 'Returns 200 if ok',
         tags: [ 'api', 'gifts' ]
     }
 })
 
 export class SendEmail implements  OnGet {
 
-    constructor(private _mailsService: MailsService) {
+    constructor(private _mailsService: MailsService, private  _logger: LoggerService) {
     }
 
 
     onGet(request: Request): Observable<void> {
-        console.log('test');
-        this._mailsService.testEmail();
-        return of(undefined);
+        return this._mailsService.testEmail(request.params.id)
+            .pipe(
+                tap( _ => this._logger.info(_))
+            );
     }
 
 }
--- src/services/mails.service/mails.service.ts
+++ src/services/mails.service/mails.service.ts
@@ -1,28 +1,44 @@
 import {Injectable} from '@hapiness/core';
 import {Config} from '@hapiness/config';
+import { Observable, of, throwError} from 'rxjs';
+import {GiftsDocumentService} from '../gifts';
+import {catchError, flatMap, tap} from 'rxjs/operators';
+import {Biim} from '@hapiness/biim';
+import {Gift} from '../../interfaces';
 
 @Injectable()
 export class MailsService {
     // To store transporter nodemailer
-    private transporter: any;
+    private _transporter: any;
+    private _gift: Gift;
 
-    constructor() {
+    constructor(private _giftsDocumentService: GiftsDocumentService) {
         let nodemailer = require('nodemailer');
-        this.transporter = nodemailer.createTransport(Config.get('mail'));
+        this._transporter = nodemailer.createTransport(Config.get('mail'));
     }
 
-    testEmail() {
+    testEmail(idGift: string): Observable<any> {
+        this._giftsDocumentService.findById(idGift)
+            .pipe(
+
+                catchError(e => throwError(Biim.preconditionFailed(e.message))),
+                flatMap(_ =>
+                    !!_ ?
+                        of(_) :
+                        throwError(Biim.notFound(`Gift with id '${idGift}' not found`))
+                )
+            ).subscribe((gift: Gift) => this._gift = gift);
         // setup email data with unicode symbols
         let mailOptions = {
             from: '"Easy Gift" <' + Config.get('mail.auth.user') + '>', // sender address
-            to: 'simonhajek88@gmail.com', // list of receivers
-            subject: 'Hello ✔', // Subject line
+            to: this._prepareEmailList(this._gift, ''), // list of receivers
+            subject: this._gift.name + 'quantity : ' + this._gift.quantity, // Subject line
             text: 'Hello world?', // plain text body
             html: '<b>Hello world?</b>' // html body
         };
 
         // send mail with defined transport object
-        this.transporter.sendMail(mailOptions, (error, info) => {
+        this._transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 return console.log(error);
             }
@@ -31,6 +47,26 @@ export class MailsService {
             // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
             // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         });
+        return of(undefined);
+    }
+
+    private _prepareEmailList(gift: Gift, emailList: string): Observable<string> {
+        return of(emailList)
+            .pipe(
+                tap( _ => {
+                    gift.listPeople.forEach(function(val, key, arr) {
+                        if ( ! val.send) {
+                            val.send = true;
+                            if (! Object.is(arr.length - 1, key)) {
+                                emailList += val.mail + ', ';
+                            } else {
+                                emailList += val.mail;
+                            }
+                        }
+                    });
+                    this._giftsDocumentService.findByIdAndUpdate(gift.id, gift);
+                })
+            );
     }
 
 
